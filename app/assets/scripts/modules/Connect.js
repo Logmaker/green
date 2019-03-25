030a16505a1d666f7fd5ac8e363d7dc2374a25b2
Moshe <firstaidabcd@gmail.com>
2019-03-25 21:55:22 +0100

    icons geplaatst

---
+++ app/assets/scripts/modules/Connect.js
@@ -0,0 +1,11 @@
+import $ from "jquery";
+
+class Connect {
+  constructor(){
+    $(".modal__form").click(function(){
+      console.log("testing ground... ");
+    });
+  }
+}
+
+export default Connect;
