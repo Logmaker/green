2ec369454586fe554aeb8ecdb64d103ec30a9fce
Moshe <firstaidabcd@gmail.com>
2019-05-28 13:32:13 +0200

    linken en tekst geplaatst

--- app/assets/scripts/modules/Connect.js
+++ app/assets/scripts/modules/Connect.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 class Connect {
   constructor(){
     $(".modal__form").click(function(){
-      console.log("testing ground... ");
+      console.log("testing ground... "); 
     });
   }
 }
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,14 +1,56 @@
+const url = require('url');
+var request = require('request');
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
+
+
 gulp.task('watch', function() {
+
+
 
   browserSync.init({
     notify: false,
     server: {
       baseDir: "app"
+    },
+    middleware: [
+    {
+        route: "/",
+        handle: function (req, res, next) {
+            // handle any requests at /
+            console.log("middleware handler is actief");
+            //console.log(req.get("/"));
+            //next();
+            console.log('Request URL:', req.originalUrl);
+            //console.log('Params:', req.get.);
+            let request_url = req.url;
+            //let data = JSON.stringify(request_url);
+            //console.log(data);
+            if (req.method === 'POST') {
+                          // Handle post info...
+                          console.log("inside post........!");
+                          let body = '';
+                      req.on('data', chunk => {
+                          body += chunk.toString(); // convert Buffer to string
+                      });
+                      req.on('end', () => {
+                          console.log(body);
+                          res.writeHead(301,{Location: 'http://192.168.1.170:3002'});
+                          res.end('ok');
+                      });
+                }
+
+
+
+
+
+
+            next();
+         }
     }
+]
   });
 
   watch('./app/index.html', function() {
